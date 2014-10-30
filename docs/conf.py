89bfd7483d386d8e4bafae4547bf13bf382f25a4
Alexey Kinyov <rudy@05bit.com>
2014-10-30 17:41:04 +0300

    Docs minor fixes

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# peewee_async documentation build configuration file, created by
+# peewee-async documentation build configuration file, created by
 # sphinx-quickstart on Sat Oct 11 20:12:24 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -55,7 +55,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'peewee_async'
+project = 'peewee-async'
 copyright = '2014, Alexey Kinev'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -188,7 +188,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'peewee_asyncdoc'
+htmlhelp_basename = 'peewee_async_doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -208,7 +208,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'peewee_async.tex', 'peewee_async Documentation',
+  ('index', 'peewee-async.tex', 'peewee-async Documentation',
    'Alexey Kinev', 'manual'),
 ]
 
@@ -238,7 +238,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'peewee_async', 'peewee_async Documentation',
+    ('index', 'peewee-async', 'peewee-async Documentation',
      ['Alexey Kinev'], 1)
 ]
 
@@ -252,8 +252,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'peewee_async', 'peewee_async Documentation',
-   'Alexey Kinev', 'peewee_async', 'One line description of project.',
+  ('index', 'peewee-async', 'peewee-async Documentation',
+   'Alexey Kinev', 'peewee-async', 'One line description of project.',
    'Miscellaneous'),
 ]
 
