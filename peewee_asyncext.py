e33c592d538512ede4ac6a64487f2dc2957ef04f
Alexey Kinyov <rudy@05bit.com>
2015-06-07 16:03:36 +0300

    Clean docstrings from wrong license lines

--- peewee_asyncext.py
+++ peewee_asyncext.py
@@ -8,19 +8,9 @@ https://github.com/05bit/peewee-async
 .. _peewee: https://github.com/coleifer/peewee
 .. _asyncio: https://docs.python.org/3/library/asyncio.html
 
-Copyright 2014 Alexey Kinev, 05Bit http://05bit.com
+Licensed under The MIT License (MIT)
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Copyright (c) 2014, Alexey Kinev <rudy@05bit.com>
 
 """
 from peewee_async import AsyncPostgresqlMixin, PooledAsyncConnection
