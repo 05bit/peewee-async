ebe362e5b2527e39ac45b57c6bb7cf926498f6b3
Alexey Kinyov <rudy@05bit.com>
2016-05-01 23:04:28 +0300

    Update long description in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,24 @@ with open('peewee_async.py') as file:
             __version__ = line.split('=')[1].strip().strip("'").strip('"')
             break
 
+long_description = \
+"""Current state: **alpha**, yet API seems fine and mostly stable.
+
+In current version (0.5.x) new-high level API is introduced while older low-level API partially marked as deprecated.
+
+* Works on Python 3.4+
+* Has support for PostgreSQL via `aiopg`
+* Has support for MySQL via `aiomysql`
+* Single point for high-level async API
+* Drop-in replacement for sync code, sync will remain sync
+* Basic operations are supported
+* Transactions support is present, yet not heavily tested
+
+The source code is hosted on `GitHub`_.
+
+.. _GitHub: https://github.com/05bit/peewee-async
+"""
+
 setup(
     name="peewee-async",
     version=__version__,
@@ -19,7 +37,7 @@ setup(
     author_email='rudy@05bit.com',
     url='https://github.com/05bit/peewee-async',
     description=__doc__,
-    # long_description=__doc__,
+    long_description=long_description,
     license='MIT',
     zip_safe=False,
     install_requires=(
