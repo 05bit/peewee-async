b31ff143220fa80b3c3751770d1d64706d3686c9
zeromake <a390720046@gmail.com>
2017-12-27 17:51:57 +0800

    code: Refactoring

---
+++ peewee_async/__init__.py
@@ -0,0 +1,45 @@
+from .manager import Manager
+from .pg import PostgresqlDatabase, PooledPostgresqlDatabase
+from .mysql import MySQLDatabase, PooledMySQLDatabase
+
+from .utils import (
+    execute,
+    count,
+    scalar,
+    get_object,
+    create_object,
+    delete_object,
+    update_object
+)
+from .transactions import atomic, transaction, savepoint
+from .sync_utils import sync_unwanted, UnwantedSyncQueryError
+
+__version__ = '0.5.10'
+
+__all__ = [
+    ### High level API ###
+
+    'Manager',
+    'PostgresqlDatabase',
+    'PooledPostgresqlDatabase',
+    'MySQLDatabase',
+    'PooledMySQLDatabase',
+
+    ### Low level API ###
+
+    'execute',
+    'count',
+    'scalar',
+    'atomic',
+    'transaction',
+    'savepoint',
+
+    ### Deprecated ###
+
+    'get_object',
+    'create_object',
+    'delete_object',
+    'update_object',
+    'sync_unwanted',
+    'UnwantedSyncQueryError',
+]
---
+++ peewee_async/ext/__init__.py
@@ -0,0 +1,88 @@
+"""
+peewee-async
+============
+
+Asynchronous interface for `peewee`_ ORM powered by `asyncio`_:
+https://github.com/05bit/peewee-async
+
+.. _peewee: https://github.com/coleifer/peewee
+.. _asyncio: https://docs.python.org/3/library/asyncio.html
+
+Licensed under The MIT License (MIT)
+
+Copyright (c) 2014, Alexey Kinëv <rudy@05bit.com>
+
+"""
+from playhouse.db_url import register_database
+
+from peewee_async import AsyncPostgresqlMixin
+import playhouse.postgres_ext as ext
+
+
+class PostgresqlExtDatabase(AsyncPostgresqlMixin, ext.PostgresqlExtDatabase):
+    """PosgreSQL database extended driver providing **single drop-in sync**
+    connection and **single async connection** interface.
+
+    JSON fields support is always enabled, HStore supports is enabled by default,
+    but can be disabled with ``register_hstore=False`` argument.
+
+    Example::
+
+        database = PostgresqlExtDatabase('test', register_hstore=False)
+
+    See also:
+    https://peewee.readthedocs.io/en/latest/peewee/playhouse.html#PostgresqlExtDatabase
+    """
+    def init(self, database, **kwargs):
+        self.min_connections = 1
+        self.max_connections = 1
+        super().init(database, **kwargs)
+        self.init_async(enable_json=True,
+                        enable_hstore=self.register_hstore)
+
+    @property
+    def use_speedups(self):
+        return False
+
+    @use_speedups.setter
+    def use_speedups(self, value):
+        pass
+
+
+register_database(PostgresqlExtDatabase, 'postgresext+async', 'postgresqlext+async')
+
+
+class PooledPostgresqlExtDatabase(AsyncPostgresqlMixin, ext.PostgresqlExtDatabase):
+    """PosgreSQL database extended driver providing **single drop-in sync**
+    connection and **async connections pool** interface.
+
+    JSON fields support is always enabled, HStore supports is enabled by default,
+    but can be disabled with ``register_hstore=False`` argument.
+
+    :param max_connections: connections pool size
+
+    Example::
+
+        database = PooledPostgresqlExtDatabase('test', register_hstore=False,
+                                               max_connections=20)
+
+    See also:
+    https://peewee.readthedocs.io/en/latest/peewee/playhouse.html#PostgresqlExtDatabase
+    """
+    def init(self, database, **kwargs):
+        self.min_connections = kwargs.pop('min_connections', 1)
+        self.max_connections = kwargs.pop('max_connections', 20)
+        super().init(database, **kwargs)
+        self.init_async(enable_json=True,
+                        enable_hstore=self.register_hstore)
+
+    @property
+    def use_speedups(self):
+        return False
+
+    @use_speedups.setter
+    def use_speedups(self, value):
+        pass
+
+
+register_database(PooledPostgresqlExtDatabase, 'postgresext+pool+async', 'postgresqlext+pool+async')
