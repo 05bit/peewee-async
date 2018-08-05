daaae62e85bce1f1ba05abb1ba09789e74e6afb9
Alexey Kinev <rudy@05bit.com>
2018-08-05 18:14:46 +0300

    Enable tests for transactions again

--- tests/__init__.py
+++ tests/__init__.py
@@ -742,95 +742,95 @@ class FakeUpdateError(Exception):
     pass
 
 
-# class ManagerTransactionsTestCase(BaseManagerTestCase):
-#     # only = ['postgres', 'postgres-ext', 'postgres-pool', 'postgres-pool-ext']
-#     only = None
+class ManagerTransactionsTestCase(BaseManagerTestCase):
+    # only = ['postgres', 'postgres-ext', 'postgres-pool', 'postgres-pool-ext']
+    only = None
 
-#     def test_atomic_success(self):
-#         """Successful update in transaction.
-#         """
-#         async def test(objects):
-#             obj = await objects.create(TestModel, text='FOO')
-#             obj_id = obj.id
+    def test_atomic_success(self):
+        """Successful update in transaction.
+        """
+        async def test(objects):
+            obj = await objects.create(TestModel, text='FOO')
+            obj_id = obj.id
 
-#             async with objects.atomic():
-#                 obj.text = 'BAR'
-#                 await objects.update(obj)
+            async with objects.atomic():
+                obj.text = 'BAR'
+                await objects.update(obj)
 
-#             res = await objects.get(TestModel, id=obj_id)
-#             self.assertEqual(res.text, 'BAR')
+            res = await objects.get(TestModel, id=obj_id)
+            self.assertEqual(res.text, 'BAR')
 
-#         self.run_with_managers(test)
+        self.run_with_managers(test)
 
-#     def test_atomic_failed(self):
-#         """Failed update in transaction.
-#         """
-#         async def test(objects):
-#             obj = await objects.create(TestModel, text='FOO')
-#             obj_id = obj.id
+    def test_atomic_failed(self):
+        """Failed update in transaction.
+        """
+        async def test(objects):
+            obj = await objects.create(TestModel, text='FOO')
+            obj_id = obj.id
 
-#             try:
-#                 async with objects.atomic():
-#                     obj.text = 'BAR'
-#                     await objects.update(obj)
-#                     raise FakeUpdateError()
-#             except FakeUpdateError as e:
-#                 error = True
-#                 res = await objects.get(TestModel, id=obj_id)
+            try:
+                async with objects.atomic():
+                    obj.text = 'BAR'
+                    await objects.update(obj)
+                    raise FakeUpdateError()
+            except FakeUpdateError as e:
+                error = True
+                res = await objects.get(TestModel, id=obj_id)
 
-#             self.assertTrue(error)
-#             self.assertEqual(res.text, 'FOO')
+            self.assertTrue(error)
+            self.assertEqual(res.text, 'FOO')
 
-#         self.run_with_managers(test)
+        self.run_with_managers(test)
 
-#     def test_several_transactions(self):
-#         """Run several transactions in parallel tasks.
-#         """
-#         wait = lambda tasks: self.loop.run_until_complete(
-#             asyncio.wait([
-#                 self.loop.create_task(t) for t in tasks
-#             ], loop=self.loop))
+    def test_several_transactions(self):
+        """Run several transactions in parallel tasks.
+        """
+        wait = lambda tasks: self.loop.run_until_complete(
+            asyncio.wait([
+                self.loop.create_task(t) for t in tasks
+            ], loop=self.loop))
 
-#         async def t1(objects):
-#             async with objects.atomic():
-#                 self.assertEqual(objects.database.transaction_depth_async(), 1)
-#                 await asyncio.sleep(0.25, loop=self.loop)
+        async def t1(objects):
+            async with objects.atomic():
+                self.assertEqual(objects.database.transaction_depth_async(), 1)
+                await asyncio.sleep(0.25, loop=self.loop)
 
-#         async def t2(objects):
-#             async with objects.atomic():
-#                 self.assertEqual(objects.database.transaction_depth_async(), 1)
-#                 await asyncio.sleep(0.0625, loop=self.loop)
+        async def t2(objects):
+            async with objects.atomic():
+                self.assertEqual(objects.database.transaction_depth_async(), 1)
+                await asyncio.sleep(0.0625, loop=self.loop)
 
-#         async def t3(objects):
-#             async with objects.atomic():
-#                 self.assertEqual(objects.database.transaction_depth_async(), 1)
-#                 await asyncio.sleep(0.125, loop=self.loop)
+        async def t3(objects):
+            async with objects.atomic():
+                self.assertEqual(objects.database.transaction_depth_async(), 1)
+                await asyncio.sleep(0.125, loop=self.loop)
 
-#         for _, objects in self.managers.items():
-#             wait([
-#                 t1(objects),
-#                 t2(objects),
-#                 t3(objects),
-#             ])
+        for _, objects in self.managers.items():
+            wait([
+                t1(objects),
+                t2(objects),
+                t3(objects),
+            ])
 
-#             with self.manager(objects, allow_sync=True):
-#                 for model in reversed(self.models):
-#                     model.delete().execute()
+            with self.manager(objects, allow_sync=True):
+                for model in reversed(self.models):
+                    model.delete().execute()
 
-#             self.run_count += 1
+            self.run_count += 1
 
-#     def test_atomic_fail_with_disconnect(self):
-#         """Database gone in transaction.
-#         """
-#         async def test(objects):
-#             error = False
-#             try:
-#                 async with objects.atomic():
-#                     await objects.database.close_async()
-#                     raise FakeUpdateError()
-#             except FakeUpdateError:
-#                 error = True
+    def test_atomic_fail_with_disconnect(self):
+        """Database gone in transaction.
+        """
+        async def test(objects):
+            error = False
+            try:
+                async with objects.atomic():
+                    await objects.database.close_async()
+                    raise FakeUpdateError()
+            except FakeUpdateError:
+                error = True
 
-#             self.assertTrue(error)
+            self.assertTrue(error)
 
-#         self.run_with_managers(test)
+        self.run_with_managers(test)
