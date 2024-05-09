6bd3c24d87f906cc4aa6d31d86c4cc01cf549bfa
Alexey Kinev <rudy@05bit.com>
2024-05-09 19:35:03 +0400

    Added draft test for compound select (feture not yet present for AioModel)

--- tests/aio_model/test_selecting.py
+++ tests/aio_model/test_selecting.py
@@ -1,9 +1,11 @@
+import peewee
+import pytest
 from tests.conftest import all_dbs
-from tests.models import TestModelAlpha, TestModelBeta
+from tests.models import TestModel, TestModelAlpha, TestModelBeta
 
 
 @all_dbs
-async def test__select__w_join(manager):
+async def test_select_w_join(manager):
     alpha = await TestModelAlpha.aio_create(text="Test 1")
     beta = await TestModelBeta.aio_create(alpha_id=alpha.id, text="text")
 
@@ -14,3 +16,22 @@ async def test__select__w_join(manager):
 
     assert result.id == beta.id
     assert result.joined_alpha.id == alpha.id
+
+
+# @pytest.mark.skip
+@all_dbs
+async def test_select_compound(manager):
+    obj1 = await manager.create(TestModel, text="Test 1")
+    obj2 = await manager.create(TestModel, text="Test 2")
+    query = (
+        TestModel.select().where(TestModel.id == obj1.id) |
+        TestModel.select().where(TestModel.id == obj2.id)
+    )
+    assert isinstance(query, peewee.ModelCompoundSelectQuery)
+    # NOTE: Two `AioModelSelect` when joining via `|` produce `ModelCompoundSelectQuery`
+    # without `aio_execute()` method, so only compat mode is available for now.
+    # result = await query.aio_execute()
+    result = await manager.execute(query)
+    assert len(list(result)) == 2
+    assert obj1 in list(result)
+    assert obj2 in list(result)
