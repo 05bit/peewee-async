70faf1ce0e8069d3b14959d6e5cbc8f8bcfb74b2
Gorshkov Nikolay <nogamemorebrain@gmail.com>
2024-07-17 16:08:57 +0500

    feat: typing result_wrappers

--- peewee_async/utils.py
+++ peewee_async/utils.py
@@ -1,5 +1,5 @@
 import logging
-from typing import TypeVar, Any
+from typing import TypeVar, Any, Protocol, Optional, Sequence
 
 try:
     import aiopg
@@ -17,5 +17,15 @@ except ImportError:
 
 __log__ = logging.getLogger('peewee.async')
 __log__.addHandler(logging.NullHandler())
+
+
+class CursorProtocol(Protocol):
+    async def fetchone(self) -> Any:
+        ...
+
+    @property
+    def description(self) -> Optional[Sequence[Any]]:
+        ...
+
 
 T_Connection = TypeVar("T_Connection", bound=Any)
