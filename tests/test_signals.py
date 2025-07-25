45c7dc3e4a395da9abfe19caffdfa3730cbb834c
Gorshkov Nikolay <nogamemorebrain@gmail.com>
2025-07-25 15:29:22 +0500

    fix

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -7,9 +7,6 @@ from tests.models import TestSignalModel
 from peewee_async.signals import AioModel, aio_pre_save, aio_post_save, aio_post_delete, aio_pre_delete, AioSignal, pre_init
 
 
-
-
-
 @contextmanager
 def _connect(signal: AioSignal , receiver: Callable[..., Coroutine[Any, Any, Any]], sender: type[AioModel]) -> Iterator[None]:
     signal.connect(receiver=receiver, sender=sender)
